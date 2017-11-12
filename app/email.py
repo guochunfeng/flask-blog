347652dc32253d66771d039b3894fe32d30893fe
guochunfeng <632131247@qq.com>
2017-11-12 23:07:17 +0800

    a

--- app/email.py
+++ app/email.py
@@ -5,9 +5,8 @@ from flask import current_app, render_template
 from flask_mail import Message
 from imp import reload
 from . import mail
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
+from email import charset
+charset.add_charset('utf-8', charset.SHORTEST, charset.BASE64, 'utf-8')
 def send_async_email(app, msg):
     with app.app_context():
         mail.send(msg)
@@ -15,7 +14,7 @@ def send_email(to, subject, template, **kwargs):
     import crash_on_ipy 
     app = current_app._get_current_object()
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
-                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to],charset='utf-8')
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
     thr = Thread(target=send_async_email, args=[app, msg])
