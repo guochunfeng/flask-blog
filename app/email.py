60ca8692a91ee5cd162eb712fa3ba4651ea1128e
guochunfeng <632131247@qq.com>
2017-11-12 22:49:30 +0800

    q

--- app/email.py
+++ app/email.py
@@ -4,6 +4,9 @@ from threading import Thread
 from flask import current_app, render_template
 from flask_mail import Message
 from . import mail
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 def send_async_email(app, msg):
     with app.app_context():
         mail.send(msg)
